4732f6d860f9cfc356f2aa7a7d357ad8a3c81612
LZMK <jomihx@gmail.com>
2023-02-17 11:44:49 +0100

    d3

--- stranica/knjiga/js/main.js
+++ stranica/knjiga/js/main.js
@@ -1,10 +1,26 @@
+let vol1;
+adresa=window.location.href;
+adresa=adresa.split("/")
 function myFunction(e){
   //let ind = podatci.map(item => item.Stranica).indexOf(e.value);
   $("#sadrzaj").html(podatci[parseInt(e.value)-1].Tekst)
   broj=parseInt(e.value)
-  document.getElementById('pdf').src = "viewer.html?file=stranice/"+broj+".pdf";
+  document.getElementById('pdf').src = "viewer.html?file=stranice/"+adresa.at(-1).replace(".html","")+"/"+broj+".pdf";
 }
-let vol1 = "podatci.csv";
+
+function promijeni(e){
+  //let ind = podatci.map(item => item.Stranica).indexOf(e.value);
+  window.open(e.value+".html");
+}
+
+if(adresa.at(-1)=="gram1.html"){
+  vol1 = "podatci.csv";
+}
+else if(adresa.at(-1)=="gram2.html"){
+  vol1 = "podatci2.csv";
+}
+
+
 
 let results;
 let filesData = []
@@ -36,7 +52,7 @@ function format(item, state) {
   if (!item.id) {
     return item.text;
   }
-  var countryUrl = "stranice/";
+  var countryUrl = "stranice/"+adresa.at(-1).replace(".html","")+"/";
   var url = state ? stateUrl : countryUrl;
   var img = $("<img>", {
     class: "img-flag",
@@ -49,6 +65,27 @@ function format(item, state) {
   span.prepend(img);
   return span;
 }
+
+
+
+function format2(item, state) {
+  if (!item.id) {
+    return item.text;
+  }
+  var countryUrl = "stranice/gramatike/";
+  var url = state ? stateUrl : countryUrl;
+  var img = $("<img>", {
+    class: "img-flag",
+    width: 26,
+    src: url + item.element.value.toLowerCase() + ".jpg"
+  });
+  var span = $("<span>", {
+    text: " " + item.text
+  });
+  span.prepend(img);
+  return span;
+}
+
 function customMatcher(params, data) {
   // Always return the object if there is nothing to compare
   if ($.trim(params.term) === '') {
@@ -78,8 +115,14 @@ $(document).ready(function() {
   });
   $("#gramatika").select2({
     templateResult: function(item) {
-      return format(item, false);
+      return format2(item, false);
     }
   })
+
+
+
+  $(document).on('keyup', '.select2-search__field', function(e){
+    e.target.value = e.target.value.toLowerCase()
+});
 
 });
