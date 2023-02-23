33fae06ca61a869cde4d45e8f78ceb13fd8a3c88
LZMK <jomihx@gmail.com>
2023-02-23 11:07:27 +0100

    ok

--- stranica/js/index.js
+++ stranica/js/index.js
@@ -17,25 +17,25 @@ function pop_up(e){
   Swal.fire({
     title: $(e).parent().attr("data-Orth"),
     html:
-    "<p class='pos podebljaj'>Vrsta riječi: <span class='vrijednost'>"+$(e).parent().attr("data-Pos")+"</span></p>"+
-    "<p class='VerbType podebljaj'>Vrsta glagola: <span class='vrijednost'>"+$(e).parent().attr("data-VerbType")+"</span></p>"+
-    "<p class='adjectiveType podebljaj'>Vrsta pridjeva: <span class='vrijednost'>"+$(e).parent().attr("data-adjectiveType")+"</span></p>"+
-    "<p class='numeralType podebljaj'>Vrsta brojeva: <span class='vrijednost'>"+$(e).parent().attr("data-numeralType")+"</span></p>"+
-    "<p class='PronounType podebljaj'>Vrsta zamjenice: <span class='vrijednost'>"+$(e).parent().attr("data-PronounType")+"</span></p>"+
-    "<p class='nounType podebljaj'>Vrsta imenice: <span class='vrijednost'>"+$(e).parent().attr("data-nounType")+"</span></p>"+
-    "<p class='Participle podebljaj'>Participle: <span class='vrijednost'>"+$(e).parent().attr("data-Participle")+"</span></p>"+
-    "<p class='Gender podebljaj'>Spol: <span class='vrijednost'>"+$(e).parent().attr("data-Gender")+"</span></p>"+
-    "<p class='Number podebljaj'>Broj: <span class='vrijednost'>"+$(e).parent().attr("data-Number")+"</span></p>"+
-    "<p class='Case podebljaj'>Padež: <span class='vrijednost'>"+$(e).parent().attr("data-Case")+"</span></p>"+
-    "<p class='inflectionType podebljaj'>Sklonidba: <span class='vrijednost'>"+$(e).parent().attr("data-inflectionType")+"</span></p>"+
-    "<p class='animacy podebljaj'>Živost: <span class='vrijednost'>"+$(e).parent().attr("data-animacy")+"</span></p>"+
-    "<p class='person podebljaj'>Lice: <span class='vrijednost'>"+$(e).parent().attr("data-person")+"</span></p>"+
-    "<p class='tense podebljaj'>Vrijeme: <span class='vrijednost'>"+$(e).parent().attr("data-tense")+"</span></p>"+
-    "<p class='tenseType podebljaj'>Glagolsko vrijeme: <span class='vrijednost'>"+$(e).parent().attr("data-tenseType")+"</span></p>"+
-    "<p class='mood podebljaj'>Glagolski način: <span class='vrijednost'>"+$(e).parent().attr("data-mood")+"</span></p>"+
-    "<p class='infinitive podebljaj'>Infinitiv: <span class='vrijednost'>"+$(e).parent().attr("data-infinitive")+"</span></p>"+
-    "<p class='verbialAdverb podebljaj'>Glagolski prilog: <span class='vrijednost'>"+$(e).parent().attr("data-verbialAdverb")+"</span></p>"+
-    "<p class='voice podebljaj'>Glagolsko stanje: <span class='vrijednost'>"+$(e).parent().attr("data-voice")+"</span></p>"+
+    "<p class='pos podebljaj'>Vrsta riječi: <span class='vrijednost'>"+$(e).parent().attr("data-Pos").replace("#","")+"</span></p>"+
+    "<p class='VerbType podebljaj'>Vrsta glagola: <span class='vrijednost'>"+$(e).parent().attr("data-VerbType").replace("#G_glavni","glavni").replace("#G_pomocni","pomoćni")+"</span></p>"+
+    "<p class='adjectiveType podebljaj'>Vrsta pridjeva: <span class='vrijednost'>"+$(e).parent().attr("data-adjectiveType").replace("#P_opisni","opisni").replace("#P_odnosni","odnosni").replace("#P_posvojni","posvojni")+"</span></p>"+
+    "<p class='numeralType podebljaj'>Vrsta brojeva: <span class='vrijednost'>"+$(e).parent().attr("data-numeralType").replace("#B_glavni","glavni").replace("#B_redni","redni").replace("#B_brojevna_imenica","brojevna imenica").replace("#B_brojevni_pridjev","brojevni pridjev")+"</span></p>"+
+    "<p class='PronounType podebljaj'>Vrsta zamjenice: <span class='vrijednost'>"+$(e).parent().attr("data-PronounType").replace("#Z_osobna","osobna").replace("#Z_pokazna","pokazna").replace("#Z_neodredjena","neodređena").replace("#Z_posvojna","posvojna").replace("#Z_upitna","upitna").replace("#Z_odnosna","odnosna").replace("#Z_povratna","povratna").replace("#Z_povratno_posvojna","povratno posvojna")+"</span></p>"+
+    "<p class='nounType podebljaj'>Vrsta imenice: <span class='vrijednost'>"+$(e).parent().attr("data-nounType").replace("#I_vlastita","vlastita").replace("#I_opca","opća")+"</span></p>"+
+    "<p class='Participle podebljaj'>Participle: <span class='vrijednost'>"+$(e).parent().attr("data-Participle").replace("#G_particip","particip").replace("#G_radni","radni").replace("#G_trpni","trpni")+"</span></p>"+
+    "<p class='Gender podebljaj'>Spol: <span class='vrijednost'>"+$(e).parent().attr("data-Gender").replace("#muski","muški").replace("#zenski","ženski").replace("#srednji","srednji")+"</span></p>"+
+    "<p class='Number podebljaj'>Broj: <span class='vrijednost'>"+$(e).parent().attr("data-Number").replace("#jednina","jednina").replace("#mnozina","množina")+"</span></p>"+
+    "<p class='Case podebljaj'>Padež: <span class='vrijednost'>"+$(e).parent().attr("data-Case").replace("#","")+"</span></p>"+
+    "<p class='inflectionType podebljaj'>Sklonidba: <span class='vrijednost'>"+$(e).parent().attr("data-inflectionType").replace("#I_a_sklonidba","<em>a</em>-sklonidba").replace("#I_e_sklonidba","<em>e</em>-sklonidba").replace("#I_i_sklonidba","<em>i</em>-sklonidba")+"</span></p>"+
+    "<p class='animacy podebljaj'>Živost: <span class='vrijednost'>"+$(e).parent().attr("data-animacy").replace("#I_zivo","živo").replace("#I_nezivo","neživo")+"</span></p>"+
+    "<p class='person podebljaj'>Lice: <span class='vrijednost'>"+$(e).parent().attr("data-person").replace("#","").replace("trece","treće")+"</span></p>"+
+    "<p class='tense podebljaj'>Vrijeme: <span class='vrijednost'>"+$(e).parent().attr("data-tense").replace("#proslo","prošlo").replace("#sadasnje","sadašnje").replace("#buduce","buduće")+"</span></p>"+
+    "<p class='tenseType podebljaj'>Glagolsko vrijeme: <span class='vrijednost'>"+$(e).parent().attr("data-tenseType").replace("#G_","").replace("_II"," II.").replace("_I"," I.")+"</span></p>"+
+    "<p class='mood podebljaj'>Glagolski način: <span class='vrijednost'>"+$(e).parent().attr("data-mood").replace("#","").replace("_II"," II.").replace("_I"," I.")+"</span></p>"+
+    "<p class='infinitive podebljaj'>Infinitiv: <span class='vrijednost'>"+$(e).parent().attr("data-infinitive").replace("#G_infinitiv","infinitiv").replace("#G_supin","supin")+"</span></p>"+
+    "<p class='verbialAdverb podebljaj'>Glagolski prilog: <span class='vrijednost'>"+$(e).parent().attr("data-verbialAdverb").replace("#G_prosli","prošli").replace("#G_sadasnji","sadašnji")+"</span></p>"+
+    "<p class='voice podebljaj'>Glagolsko stanje: <span class='vrijednost'>"+$(e).parent().attr("data-voice").replace("#","").replace("mnozina","množina")+"</span></p>"+
     "<p class='Fajl podebljaj'>Gramatika: <span class='vrijednost'>"+$(e).parent().attr("data-Fajl")+"</span></p>",
     showCloseButton: true,
     focusConfirm: false,
@@ -49,7 +49,6 @@ function pop_up(e){
       $(this).text($(this).text().substring(1)) 
     }
  });
-
 }
 var podatci
 var natuknice = []
@@ -63,7 +62,7 @@ setTimeout(() => {
   }
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
-}, 1500);
+}, 1000);
 
 atributi = []
 function myFunction(e) {
@@ -86,7 +85,8 @@ function trazilica() {
     if (option.value != "")
       atributi.push(option.value);
   });
- 
+  document.getElementById("loader").style.display = "block";
+  setTimeout(function() {
   for (let i = 0; i < podatci.length; i++) {
     var obj = podatci[i];
     lista_svega = [obj.Orth, obj.Pos, obj.VerbType, obj.Participle, obj.Gender, obj.PronounType, obj.Number, obj.Case, obj.inflectionType, obj.animacy, obj.tense,obj.person, obj.adjectiveType, obj.numeralType, obj.tenseType, obj.voice, obj.nounType, obj.mood, obj.infinitive, obj.verbialAdverb, obj.Fajl]
@@ -98,4 +98,6 @@ function trazilica() {
    
   }
   if(resultList.innerHTML==""){resultList.innerHTML="Nema rezultata za zadanja polja pretraživanja."}
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";})
 }
